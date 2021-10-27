078ed86625727f06851621d3e298c98f4793486b
alever_sn <759636538@qq.com>
2021-10-27 10:09:31 +0800

    [update]

--- t1.py
+++ t1.py
@@ -8,7 +8,7 @@ args = {
     'num_epochs': 30,
     'num_gpus': [0, 1, 2, 3],
     'bert_config_file_name': './model/chinese_wwm_ext/bert_config.json',
-    'pretrained_file_name': './save_pretrained/Weibo_Pretrained/Bert_43/pytorch_model.bin',
+    'pretrained_file_name': './save_pretrained/Pre_trained/Bert_55580/pytorch_model.bin',
     'hidden_dim': 300,
     'max_seq_length': 150,
     'max_scan_num': 1000000,
