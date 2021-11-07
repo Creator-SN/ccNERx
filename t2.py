a7efa9f61194fca1084955404a9f5491d177a1e6
minskiter <minskiter@qq.com>
2021-11-07 15:05:50 +0800

    [reset] delete some records

--- t2.py
+++ t2.py
@@ -108,10 +108,11 @@ args = {
     'num_epochs': 30,
     'num_gpus': [0, 1, 2, 3],
     'bert_config_file_name': 'model/chinese_wwm_ext/bert_config.json',
+    # 'bert_config_file_name': 'save_pretrained/weibo_x20_pretrained/Bert_2910/config.json',
     'hidden_dim': 300,
     'max_seq_length': 150,
     'max_scan_num': 1000000,
-    'train_file': './data/weibo/train.json',
+    'train_file': './data/weibonew/train_20_epoch.json',
     'eval_file': './data/weibo/dev.json',
     'test_file': './data/weibo/test.json',
     'bert_vocab_file': 'model/chinese_wwm_ext/vocab.txt',
@@ -125,8 +126,9 @@ args = {
     'eval_batch_size': 64,
     'do_shuffle': True,
     'model_name': 'LEBert',
-    'task_name': 'weibo_task_1_f1_score_verify_2',
+    'task_name': 'weibo_20_epoch_GPT2',
     'pretrained_file_name':'model/chinese_wwm_ext/pytorch_model.bin'
+    # 'pretrained_file_name':'save_pretrained/weibo_x20_pretrained/Bert_2910/pytorch_model.bin'
 }
 
 from CC.trainer import NERTrainer
@@ -135,7 +137,71 @@ trainer = NERTrainer(**args)
 for i in trainer():
     a = i
 
+
+# %%
+from CC.loaders import *
+from tqdm import *
+from CC.loaders.utils import *
+import json
+from CC.pre_trained import NERPreTrainer
+
+args = {
+    'num_epochs': 30,
+    'num_gpus': [0, 1, 2, 3],
+    'bert_config_file_name': './model/chinese_wwm_ext/bert_config.json',
+    'pretrained_file_name': './model/chinese_wwm_ext/pytorch_model.bin',
+    'max_seq_length': 512,
+    'max_scan_num': 1000000,
+    'train_file': './data/weibonew/train_20.json',
+    'eval_file': './data/weibo/dev.json',
+    'test_file': './data/weibo/test.json',
+    'bert_vocab_file': './model/chinese_wwm_ext/vocab.txt',
+    'tag_file': './data/weibo/pretrained_labels.txt',
+    'loader_name': 'lex_loader',
+    "word_embedding_file": "./data/tencent/word_embedding.txt",
+    "word_vocab_file": "./data/tencent/tencent_vocab.txt",
+    "word_vocab_file_with_tag": "./data/tencent/tencent_vocab_with_tag.json",
+    "default_tag": "O",
+    'batch_size': 32,
+    'eval_batch_size': 64,
+    'do_shuffle': True,
+    'task_name': 'weibo_x20_pretrained',
+    "use_gpu": True,
+    "debug": True,
+    "tag_rules": {
+        "PER.NOM": "人的象征",
+        "LOC.NAM": "地点",
+        "PER.NAM": "人",
+        "GPE.NAM": "政治实体",
+        "ORG.NAM": "组织",
+        "ORG.NOM": "组织的象征",
+        "LOC.NOM": "地点的象征",
+        "GPE.NOM": "政治实体的象征",
+        "ORG": "组织",
+        "LOC": "地点",
+        "PER": "人",
+        "Time": "时间",
+        "Thing": "物品",
+        "Metric": "测量单位",
+        "Abstract": "作品",
+        "Physical": "实体",
+        "Term": "术语",
+        "company": "企业",
+        "name": "名字",
+        "game": "游戏",
+        "movie": "电影",
+        "position": "职位",
+        "address": "地址",
+        "government": "政府",
+        "scene": "景点",
+        "book": "书名"
+    }
+}
+
+pre_trainer = NERPreTrainer(**args)
+
+for i in pre_trainer():
+    a = i
+
 exit()
-
-
 # %%
