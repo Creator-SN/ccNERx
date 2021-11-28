34e83472fc29795ce400e79b3ccdebb88f0736cd
minskiter <minskiter@qq.com>
2021-11-28 22:01:17 +0800

    [update] gpt_predictor.py - update predict, support batch

--- CC/gpt_predictor.py
+++ CC/gpt_predictor.py
@@ -9,9 +9,17 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer, GPT2Config, get_linear_
 from tqdm import tqdm
 from torch.autograd import Variable
 
-class Predictor(IPredict):
 
-    def __init__(self, tokenizer, model_dir, padding_length=128, resume_path=False, gpu=[0], repeat_punish=1.0, top_k=8, top_p=0.5):
+class Predictor(IPredict):
+    def __init__(self,
+                 tokenizer,
+                 model_dir,
+                 padding_length=128,
+                 resume_path=False,
+                 gpu=[0],
+                 repeat_punish=1.0,
+                 top_k=8,
+                 top_p=0.5):
         self.tokenizer = tokenizer
         self.padding_length = padding_length
         self.model_init(model_dir)
@@ -26,72 +34,107 @@ class Predictor(IPredict):
             print('Accessing Resume PATH: {} ...\n'.format(resume_path))
             model_dict = torch.load(resume_path).module.state_dict()
             self.model.load_state_dict(model_dict)
-        
+
         if torch.cuda.is_available() and self.model_cuda == False:
-            self.model = torch.nn.DataParallel(self.model, device_ids=gpu).cuda()
+            self.model = torch.nn.DataParallel(self.model,
+                                               device_ids=gpu).cuda()
             self.model_cuda = True
             self.model.to(device)
-        
+
         self.model.to(device)
-    
+
     def model_init(self, model_dir):
-        self.model = GPT2LMHeadModel.from_pretrained(
-            model_dir)
+        self.model = GPT2LMHeadModel.from_pretrained(model_dir)
         self.model_cuda = False
-    
+
     def data_process(self, *X):
         src = ''
         for item in X:
             src = src + self.tokenizer.sep_token + item if src != '' else item
-        T = self.tokenizer(src, add_special_tokens=True, max_length=self.padding_length, truncation=True)
+        T = self.tokenizer(src,
+                           add_special_tokens=True,
+                           max_length=self.padding_length,
+                           truncation=True)
         input_ids = torch.tensor(T['input_ids'])
         attn_mask = torch.tensor(T['attention_mask'])
 
-        return {
-            'input_ids': input_ids,
-            'attention_mask': attn_mask
-        }
-    
-    def top_k_top_p_filtering(self, logits, top_k=0, top_p=0.0, filter_value=-float("Inf")):
-        """Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
-        Args:
-            logits: logits distribution shape (vocabulary size)
-            top_k > 0: keep only top k tokens with highest probability (top-k filtering).
-            top_p > 0.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
-                Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
-        From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
-        """
-        assert (
-            logits.dim() == 1
-        )  # batch size 1 for now - could be updated for more but the code would be less clear
-        top_k = min(top_k, logits.size(-1))  # Safety check
+        return {'input_ids': input_ids, 'attention_mask': attn_mask}
+
+    def top_k_top_p_filtering(self,
+                              logits,
+                              top_k=0,
+                              top_p=0.0,
+                              filter_value=-float("Inf")):
+        assert logits.dim() == 2
+        top_k = min(top_k, logits[0].size(-1))  # Safety check
         if top_k > 0:
             # Remove all tokens with a probability less than the last token of the top-k
             # torch.topk返回值和索引
-            indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+            indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1,
+                                                                      None]
             logits[indices_to_remove] = filter_value
 
         if top_p > 0.0:
             sorted_logits, sorted_indices = torch.sort(logits, descending=True)
             # 按维度里元素顺序累加并按顺序输出累加值
-            cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+            cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1),
+                                            dim=-1)
 
             # Remove tokens with cumulative probability above the threshold
             # 计算出累加概率大于p的索引
-            sorted_indices_to_remove = cumulative_probs > top_p
+            sorted_indices_to_remove = cumulative_probs > self.cuda(
+                torch.tensor([[top_p]] * logits.shape[0]))
             # Shift the indices to the right to keep also the first token above the threshold
             # 右移索引确保第一个token不会被1到
-            sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+            sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+                ..., :-1].clone()
             sorted_indices_to_remove[..., 0] = 0
-            
             # 这种操作会筛选出sorted_indices_to_remove为1的值
-            indices_to_remove = sorted_indices[sorted_indices_to_remove]
-            logits[indices_to_remove] = filter_value
+            for i, indices in enumerate(sorted_indices_to_remove):
+                indices_to_remove = sorted_indices[i][indices]
+                logits[i][indices_to_remove] = filter_value
         return logits
-    
+
+    # def top_k_top_p_filtering(self, logits, top_k=0, top_p=0.0, filter_value=-float("Inf")):
+    #     """Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
+    #     Args:
+    #         logits: logits distribution shape (vocabulary size)
+    #         top_k > 0: keep only top k tokens with highest probability (top-k filtering).
+    #         top_p > 0.0: keep the top tokens with cumulative probability >= top_p (nucleus filtering).
+    #             Nucleus filtering is described in Holtzman et al. (http://arxiv.org/abs/1904.09751)
+    #     From: https://gist.github.com/thomwolf/1a5a29f6962089e871b94cbd09daf317
+    #     """
+    #     assert (
+    #         logits.dim() == 1
+    #     )  # batch size 1 for now - could be updated for more but the code would be less clear
+    #     top_k = min(top_k, logits.size(-1))  # Safety check
+    #     if top_k > 0:
+    #         # Remove all tokens with a probability less than the last token of the top-k
+    #         # torch.topk返回值和索引
+    #         indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+    #         logits[indices_to_remove] = filter_value
+
+    #     if top_p > 0.0:
+    #         sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+    #         # 按维度里元素顺序累加并按顺序输出累加值
+    #         cumulative_probs = torch.cumsum(F.softmax(sorted_logits, dim=-1), dim=-1)
+
+    #         # Remove tokens with cumulative probability above the threshold
+    #         # 计算出累加概率大于p的索引
+    #         sorted_indices_to_remove = cumulative_probs > top_p
+    #         # Shift the indices to the right to keep also the first token above the threshold
+    #         # 右移索引确保第一个token不会被1到
+    #         sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[..., :-1].clone()
+    #         sorted_indices_to_remove[..., 0] = 0
+
+    #         # 这种操作会筛选出sorted_indices_to_remove为1的值
+    #         indices_to_remove = sorted_indices[sorted_indices_to_remove]
+    #         logits[indices_to_remove] = filter_value
+    #     return logits
+
     def __call__(self, *X):
         return self.predict(*X)
-    
+
     def predict(self, *X):
         it = self.data_process(*X)
         for key in it.keys():
@@ -102,7 +145,7 @@ class Predictor(IPredict):
             text = pred.max(-1)[1].tolist()
 
         return pred.cpu(), text, it['input_ids'].tolist()
-    
+
     def predict_continous(self, *X):
         it = self.data_process(*X, self.tokenizer.sep_token)
         for key in it.keys():
@@ -115,16 +158,59 @@ class Predictor(IPredict):
                 for id in it['input_ids'][0]:
                     next_token_logits[id] /= self.repeat_punish
                 filtered_logits = self.top_k_top_p_filtering(
-                    next_token_logits, top_k=self.top_k, top_p=self.top_p
-                )
-                next_token = torch.multinomial(
-                    torch.softmax(filtered_logits, dim=-1), num_samples=1
-                )
-                it['input_ids'] = torch.cat((it['input_ids'], next_token.unsqueeze(0)), dim=1)
-                it['attention_mask'] = torch.cat((it['attention_mask'], next_token.gt(0).unsqueeze(0)), dim=1)
-
+                    next_token_logits.unsqueeze(0),
+                    top_k=self.top_k,
+                    top_p=self.top_p)[0]
+                next_token = torch.multinomial(torch.softmax(filtered_logits,
+                                                             dim=-1),
+                                               num_samples=1)
+                it['input_ids'] = torch.cat(
+                    (it['input_ids'], next_token.unsqueeze(0)), dim=1)
+                it['attention_mask'] = torch.cat(
+                    (it['attention_mask'], next_token.gt(0).unsqueeze(0)),
+                    dim=1)
                 yield pred.cpu(), it['input_ids'].tolist()
-    
+
+    def predict_continous_x(self, *X):
+        it = self.tokenizer(X,
+                            padding="max_length",
+                            add_special_tokens=True,
+                            max_length=self.padding_length,
+                            truncation=True,
+                            return_tensors="pt")
+        with torch.no_grad():
+            while True:
+                r = self.model(**it)
+                indexes = torch.sum(it["attention_mask"], dim=-1)
+                select_logits = []
+                exit_loop = True
+                # TODO: torch 是否有内置函数
+                for i, idx in enumerate(indexes):
+                    # idx out of range?
+                    if idx < it["input_ids"][0].shape[0]:
+                        select_logits.append(r.logits[i][idx])
+                        exit_loop = False
+                    else:
+                        select_logits.append(r.logits[i][idx - 1])
+                if exit_loop:
+                    break
+                logits = torch.stack(select_logits)
+                # 施加重复惩罚
+                # TODO: 如何使用torch转换
+                for i, input_ids in enumerate(it["input_ids"]):
+                    for id in input_ids[:indexes[i]]:
+                        logits[i][id] /= self.repeat_punish
+                filtered_logits = self.top_k_top_p_filtering(logits, 8, 0.5)
+
+                next_token = torch.multinomial(torch.softmax(filtered_logits,
+                                                            dim=-1),
+                                            num_samples=1)
+                for i, idx in enumerate(indexes):
+                    if idx < it["input_ids"][0].shape[0]:
+                        it["input_ids"][i][idx] = next_token[i]
+                        it["attention_mask"][i][idx] = 1
+                yield r.logits.cpu(), it["input_ids"].tolist()
+
     def cuda(self, inputX):
         if type(inputX) == tuple:
             if torch.cuda.is_available():
